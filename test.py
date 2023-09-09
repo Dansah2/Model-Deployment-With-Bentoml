579aaddfbf52c69e0feb341eea566f2df1fd3cb1
Dyimah Ansah <hdyimah@yahoo.com>
2023-09-09 01:47:04 +0000

    test model

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import bentoml
 
-pytorch_runner = bentoml.pytorch.get("pytorch_model:latest").to_runner()
-pytorch_runner.init_local()
-print(pytorch_runner.predict.run([[]]))
+runner = bentoml.pytorch.get("pytorch_model:xzrlgzsovglrsycf").to_runner()
+runner.init_local()
+print(runner.predict.run([[133.171875, 59.716081, 0.043133, -0.703383, 54.917224, 70.084438, 0.749798, -0.649512]]))
