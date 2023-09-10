c55dc3c2b396aed723f69b315bcef0feba61c949
Dyimah Ansah <hdyimah@yahoo.com>
2023-09-10 22:12:12 +0000

    updated test.py

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
 import bentoml
+import torch
 
 runner = bentoml.pytorch.get("pytorch_model:latest").to_runner()
 runner.init_local()
-print(runner.run([[133.171875, 59.716081, 0.043133, -0.703383, 54.917224, 70.084438, 0.749798, -0.649512]]))
+test_data = [[133.171875, 59.716081, 0.043133, -0.703383, 54.917224, 70.084438, 0.749798, -0.649512]]
+test_data = torch.tensor(test_data)
+print(runner.run(test_data))
