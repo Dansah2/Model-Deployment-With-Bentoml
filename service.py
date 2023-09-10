52ca0bd97f834e4c76b4f85f6cf8d81e3bb62a2a
Dyimah Ansah <hdyimah@yahoo.com>
2023-09-10 22:52:48 +0000

    updated output type

--- service.py
+++ service.py
@@ -8,7 +8,7 @@ pytorch_runner = bentoml.pytorch.get("pytorch_model:latest").to_runner()
 svc = bentoml.Service(name="pytorch_model", runners=[pytorch_runner])
 
 @svc.api(input=NumpyNdarray(), output=NumpyNdarray())
-def classify(input_series: np.ndarray) -> torch.tensor:
+def classify(input_series: np.ndarray) -> np.ndarray:
    input_series = torch.tensor(input_series)
    result = pytorch_runner.run(input_series)
    return result
